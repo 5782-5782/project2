d204ba3b42c34eb3a3ea44524b80222d2bab3712
5782-5782 <166039577+5782-5782@users.noreply.github.com>
2025-04-30 17:25:18 +0300

    Update CupUp1.py

--- CupUp1.py
+++ CupUp1.py
@@ -8,7 +8,7 @@ from telethon.sessions import StringSession
 
 API_ID=25980590
 API_HASH='1d281e8e6c14983eecc5aadabf981137'
-SESSION = '1ApWapzMBu8PQ-AH74JwzGiDk-8szOKAgE-gX9NikTKSRXwY-MQzXAHZ53UWPmcJL51fJ9nOqaVhDikTfmGaHH7wFiyXiyu1jcBV-rTd3yjpblZl5mAy5cSqnNzJ7W50FpC7pEX95sf0gZp664G5mvGMXaJwdzPP4JjWlz7vBRqcsoy_fs_aP2iQKYSoe-vz1fPQodrzLHK9bz-3Ll00Rc4nYncesTTSXUqp7jTbXoLoGYYyRdLF0LbJpvKhl6hnaoa-Ne1QFCoCCAY-rDo2uox2iMYb9Js72XOaG2I72y1G-tozPkg9I8jy2dLMv_mvOtLg3nIas9FL-fo4czEEGaFQ2Ql00BGM='
+SESSION   = '1ApWapzMBu8PQ-AH74JwzGiDk-8szOKAgE-gX9NikTKSRXwY-MQzXAHZ53UWPmcJL51fJ9nOqaVhDikTfmGaHH7wFiyXiyu1jcBV-rTd3yjpblZl5mAy5cSqnNzJ7W50FpC7pEX95sf0gZp664G5mvGMXaJwdzPP4JjWlz7vBRqcsoy_fs_aP2iQKYSoe-vz1fPQodrzLHK9bz-3Ll00Rc4nYncesTTSXUqp7jTbXoLoGYYyRdLF0LbJpvKhl6hnaoa-Ne1QFCoCCAY-rDo2uox2iMYb9Js72XOaG2I72y1G-tozPkg9I8jy2dLMv_mvOtLg3nIas9FL-fo4czEEGaFQ2Ql00BGM='
 
 logging.basicConfig(level=logging.INFO)
 
@@ -16,13 +16,13 @@ client = TelegramClient(StringSession(SESSION), API_ID, API_HASH, system_version
 pers=[]
 
 state=0
-tume=200
+tume=132
 
 async def pon():
     global state, pers
     bot = await client.get_entity('CupLegendBot')
     i = 0
-    perso = ["Пудель2", "Пудель3", "Пудель4", "Пудель5", "Пудель6", "Пудель7", "Пудель8", "Пудель9", "Пудель10", "Пудель"]
+    perso = ["MORGENSHTERN", "Леон", "Крейзи", "Голубь", "Ямаль", "Карти", "Тиг", "Слитые", "Биток", "Легенд"]
     await client.send_message(bot, "ВНИМАНИЕ!!!!\nНЕ ИСПОЛЬЗУЙТЕ БОТА НИКАКИМ ОБРАЗОМ ПО КРАЙНЕЙ МЕРЕ 2-3 МИНУТЫ!!!!\nНЕ НАЖИМАЙТЕ КНОПКИ И НЕ ПИШИТЕ ЛЮБЫЕ СООБЩЕНИЯ БОТУ!!!\nЕСЛИ ВЫ СЛУЧАЙНО ЭТО СДЕЛАЛИ, ПЕРЕЗАПУСТИТЕ СКРИПТ!!!")
     await asyncio.sleep(1)
     while i < 10:
@@ -41,39 +41,23 @@ async def pon():
 @client.on(events.NewMessage)
 async def my_event(event):
     global state
-    if event.message.text=="/start_cup" and state==0:
+    if event.message.text=="/start_cup" and state==0 and event.message.to_dict()['from_id']['user_id']==1817889040:
         state=1
         await pon()
         await cup_up()
-    if event.message.text=="/stop_cup" and state==1:
+    if event.message.text=="/stop_cup" and state==1 and event.message.to_dict()['from_id']['user_id']==1817889040:
         state=0
 
 
 async def cup_up():
     global state, tume
-    i=9
     while state==1:
-        i=i+1
         date1=int(time.time())
         await cup_up2()
         date2=int(time.time())
         date=date2-date1
         print(date)
-        await asyncio.sleep(tume+3-date)
-        if i==10:
-            i=0
-            bot = await client.get_entity('CupLegendBot')
-            await client.send_message(bot, "/cup_up")
-            await asyncio.sleep(1)
-            await client.send_message(bot, "/cup_up")
-            msgs = await client.get_messages('CupLegendBot', 1)
-            msg=msgs[0]
-            try:
-                tume=int(msg.text.split("Подожди ещё 00:")[1].split(":")[0])*60+int(msg.text.split("Подожди ещё 00:")[1].split(".")[0].split(":")[1])
-            except:
-                print("error")
-                tume=250
-                i=9
+        await asyncio.sleep(tume-date)
             
 
 async def cup_up2():
